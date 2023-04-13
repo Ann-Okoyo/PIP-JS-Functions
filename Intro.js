e50e4967cb7395a0c0daef7826cbf65381e747c4
Muyale Okoyo <annmuyale@gmail.com>
2023-04-13 19:42:10 +0300

    first commit

---
+++ Intro.js
@@ -0,0 +1,75 @@
+function greeting(){
+    console.log("Hello world");
+}
+greeting();
+greeting();
+
+function school(name){
+    console.log("AkiraChix")
+    console.log("Alliance Girls")
+
+}
+school("AkiraChix")
+school("Alliance Girls")
+
+function numbers(num1,num2){
+    console.log(num1*num2)
+}
+numbers(20,2);
+numbers(40,7);
+
+function digits(num3,num4){
+    console.log(num3/num4);
+
+
+}
+digits(10,7);
+digits(20,2);
+
+
+const number=71;
+
+function myFunc(){
+     console.log(number)
+}
+// console.log(number)
+myFunc()
+
+let numeral=77;
+
+function doIt(){
+    console.log(numeral)
+}
+doIt();
+console.log(numeral);
+//by using doIt and numeral makes them run
+
+
+let music = "22"
+
+function produce(){
+    console.log()
+}
+// fun
+ready();
+function ready(){
+    console.log("I am a future software developer")
+}
+
+
+
+let students4 ="Ann Muyale";
+
+function students1(){
+    console.log(students4)
+}
+students1();
+console.log(students4)
+
+function doSomething(){
+    console.log("Declare something.")
+}
+doSomething();
+console.log("Delare me");
+///were theres no variable connecting to it the 
+// console log print exactly what is you assign it to
